14076629cc58e1a3e71f86fb9eaa10fad2e307e2
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-02-28 12:09:43 -0300

    first commit

---
+++ src/main/java/br/com/learnspringforge/learnspringforge/LearnSpringForgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.learnspringforge.learnspringforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/learnspringforge/learnspringforge/LearnSpringForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learnspringforge.learnspringforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
